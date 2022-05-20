a8a24379beb04f1a5eafb53b85db01a7c3a3fc7b
knika-starkey <knika787@gmail.com>
2022-05-20 14:24:23 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+$(function () {
+  const phrases = [
+    "Наш менеджер перезвонит Вам в ближайшее время!",
+    "Уточнить детали можно по телефону 123456789",
+    "Оставайтесь на связи!",
+    "Сегодня прекрасная погода!",
+    "С Вами очень приятно общаться!",
+  ];
+  const hello = "Привет, я бот Пятница";
+  const goodbye = "До свидания!";
+  const bye = "Пока!";
+
+  function rPhrases() {
+    return phrases[Math.floor(Math.random() * phrases.length)];
+  }
+
+  $("h1").css("color", "red");
+  $("#answers").append(`<div class="bot_answ">${hello}</div>`);
+
+  $("#chatbot").click(function () {
+    $(this).toggleClass("show");
+  });
+  $("#answers").click(function () {
+    return false;
+  });
+  $("#question").click(function () {
+    return false;
+  });
+  $("#ok").click(function () {
+    let q = $("#question").val();
+    $("#question").val("");
+    //alert(q);
+    if (q.trim() != "") {
+      $("#answers").append(`<div class="human_answ">${q.trim()}</div>`);
+      //q = q.toLowerCase();
+      let byeLowerCase = bye.substring(0, bye.length - 1).toLowerCase();
+      setTimeout(function () {
+        if (q.toLowerCase().search(byeLowerCase) != -1) {
+          $("#answers").append(`<div class="bot_answ">${bye}</div>`);
+        } else {
+          $("#answers").append(`<div class="bot_answ">${rPhrases()}</div>`);
+        }
+        let chatbot = document.getElementById("chatbot");
+        $("#chatbot").animate(
+          {
+            scrollTop: chatbot.scrollHeight - chatbot.clientHeight,
+          },
+          500
+        );
+      }, 1000);
+    }
+
+    return false;
+  });
+  $("#question").keypress("keyup", function (event) {
+    if (event.keyCode == 13) {
+      $("#ok").click();
+      return false;
+    }
+  });
+});
